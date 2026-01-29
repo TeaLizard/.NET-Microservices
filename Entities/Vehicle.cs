a20839e9243bb3c41384194f6fabf9c808d524ba
TeaLizard <owentwilliams7@gmail.com>
2026-01-29 18:08:43 -0500

    vehicle patch

--- Entities/Vehicle.cs
+++ Entities/Vehicle.cs
@@ -2,9 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 public class Vehicle
 {
-    int? Id;
+    int Id { get; set; }
+
     [Range(1, int.MaxValue, ErrorMessage = "Vehicle must have at least 1 wheel.")]
-    int? NumberOfWheels;
-    int? NumberOfDoors;
-    string Brand = "";
+    int? NumberOfWheels { get; set; }
+
+    int? NumberOfDoors { get; set; }
+
+    string Brand { get; set; } = "";
 }
