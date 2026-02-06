3bc8a69c9f8cc67b8546d4770b011dac29019fcf
TeaLizard <owentwilliams7@gmail.com>
2026-02-06 00:33:00 -0500

    readme filled

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,14 +8,5 @@ namespace Assignment1.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
         public DbSet<Vehicle> Vehicles { get; set; }
-
-        // protected override void OnModelCreating(ModelBuilder builder)
-        // {
-        //     base.OnModelCreating(builder);
-
-        //     builder.Entity<Vehicle>().HasData(
-        //         new Vehicle { }
-        //     );
-        // }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+Vehicle Manager
+
 .NET Microservices CRUD app
+
+Owen Williams 8932017
+
+Domain vehicle JSON example:
+Number of wheels cannot be less than 1
+{
+    "id": 2,
+    "numberOfWheels": 4,
+    "numberOfDoors": 4,
+    "brand": "Ford"
+}
+
+endpoints:
+
+PORT:5062
+
+get all is GET
+
+get one is GET /{id}
+
+add one is POST with JSON body
+
+update one is PUT /{id} with JSON body
+
+delete one is DELETE /{id}
